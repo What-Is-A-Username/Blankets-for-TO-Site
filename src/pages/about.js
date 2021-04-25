bb0194a935aed484829ffd87fc7d92696e60d162
What-Is-A-Username <49571870+What-Is-A-Username@users.noreply.github.com>
2021-04-25 16:06:32 -0400

    Minor reformating

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,45 +8,45 @@ import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import styles from '../page-styles/about.module.css'
 
 class About extends React.Component {
-    render() {
-        const aboutPage = get(this, 'props.data.allContentfulOrganizationInformationAboutPageRichTextNode.edges')
-        const node = aboutPage[0].node;
+	render() {
+		const aboutPage = get(this, 'props.data.allContentfulOrganizationInformationAboutPageRichTextNode.edges')
+		const node = aboutPage[0].node;
 
-        return (
-            <Layout location={this.props.location}>
-                <SEO title="About"
-                  description="Read more about Blankets for T.O., including its primary objective of helping and advocating for the homeless through charitable events, donations, and awareness initiatives."/>
-                <div className="white-background">
-                    <div className="wrapper">
-                    {/* Description, centered  */}
-                    <h2>Our Organization</h2>
-                    <div className={"richText" + ' ' + styles.description} >
-                    <div>
-                        {node.json !== undefined ? documentToReactComponents(node.json) : <p>Error: Articles not found.</p>}
-                    </div>
-                    </div>
-                    </div>
-                </div>
-            </Layout>
-        )
-    }
+		return (
+			<Layout location={this.props.location}>
+				<SEO title="About"
+					description="Read more about Blankets for T.O., including its primary objective of helping and advocating for the homeless through charitable events, donations, and awareness initiatives." />
+				<div className="white-background">
+					<div className="wrapper">
+						{/* Description, centered  */}
+						<h2>Our Organization</h2>
+						<div className={"richText" + ' ' + styles.description} >
+							<div>
+								{node.json !== undefined ? documentToReactComponents(node.json) : <p>Error: Articles not found.</p>}
+							</div>
+						</div>
+					</div>
+				</div>
+			</Layout>
+		)
+	}
 }
 
 export default About
 
 export const aboutPageQuery = graphql`
-  query AboutQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allContentfulOrganizationInformationAboutPageRichTextNode {
-        edges {
-          node {
-            json
-          }
-        }
-      }
-  }
+	query AboutQuery {
+		site {
+			siteMetadata {
+				title
+			}
+		}
+		allContentfulOrganizationInformationAboutPageRichTextNode {
+				edges {
+					node {
+						json
+					}
+				}
+			}
+	}
 `
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -13,134 +13,133 @@ import { filter } from 'lodash'
 
 class BlogIndex extends React.Component {
 
-  render() {
-    const posts = get(this, 'props.data.allContentfulBlogPost.edges')
-    const searchQuery = get(this, 'props.location.search');
-    const searchParams = QueryString.parse(searchQuery); 
+	render() {
+		const posts = get(this, 'props.data.allContentfulBlogPost.edges')
+		const searchQuery = get(this, 'props.location.search');
+		const searchParams = QueryString.parse(searchQuery);
 
-    const clickTag = (clicked) =>
-    {
-      if (searchParams.tags.includes(clicked)) // if unselecting 
-        searchParams.tags = searchParams.tags.filter(tag => (tag != clicked)); 
-      else 
-        searchParams.tags.push(clicked); 
-      let new_query = this.props.location.origin + this.props.location.pathname + "?" + QueryString.stringify({tags: searchParams.tags, sort: searchParams.sort}, {arrayFormat: 'comma'});
-      navigate(new_query);
-    }
+		var allTags = []
+		posts.forEach(post => allTags = allTags.concat(post.node.tags));
+		const uniqueTags = allTags.filter((t, i, s) => s.indexOf(t) === i);
 
-    // filter only unique tags 
-    // https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates
-    var allTags = []
-    posts.forEach(post => allTags = allTags.concat(post.node.tags));
-    const uniqueTags = allTags.filter((t, i, s) => s.indexOf(t) === i);
+		if (searchParams['tags'] === undefined) searchParams['tags'] = [];
+		else if (typeof (searchParams['tags']) === 'string') searchParams['tags'] = [searchParams['tags']]
+		if (searchParams['sort'] === undefined) searchParams['sort'] = 'recent';
+		searchParams.tags = searchParams.tags.filter(tag => uniqueTags.includes(tag));
 
-    if (searchParams['tags'] === undefined) searchParams['tags'] = [];
-    else if (typeof(searchParams['tags']) === 'string') searchParams['tags'] = [searchParams['tags']] 
-    if (searchParams['sort'] === undefined) searchParams['sort'] = 'recent'; 
-    // only accept tags that exist in our posts
-    searchParams.tags = searchParams.tags.filter(tag => uniqueTags.includes(tag)); 
+		const meetsFilter = (post) => {
+			if (searchParams.tags.length == 0) return true;
+			for (var i = 0; i < searchParams.tags.length; i++)
+				if (!post.tags.incldues(searchParams.tags[i])) return false;
+			return false;
+		}
 
-    const meetsFilter = (post) => { 
-      if (searchParams.tags.length == 0) return true;
-      for (var i = 0; i < post.tags.length; i++)
-        if (searchParams.tags.includes(post.tags[i])) return true;
-      return false;
-    }
-    
-    const comparePosts = (a, b) => {
-      a = a.node; b = b.node;
-      if (searchParams.sort === 'recent')
-      return b.publishDateValue.localeCompare(a.publishDateValue); 
-      else if (searchParams.sort === 'oldest')
-      return a.publishDateValue.localeCompare(b.publishDateValue); 
-      if (searchParams.sort === 'titleaz')
-      return a.title.localeCompare(b.title); 
-      else if (searchParams.sort === 'titleza')
-      return b.title.localeCompare(a.title);
-      else
-        return 0; 
-    }
+		const comparePosts = (a, b) => {
+			a = a.node; b = b.node;
+			if (searchParams.sort === 'recent')
+				return b.publishDateValue.localeCompare(a.publishDateValue);
+			else if (searchParams.sort === 'oldest')
+				return a.publishDateValue.localeCompare(b.publishDateValue);
+			if (searchParams.sort === 'titleaz')
+				return a.title.localeCompare(b.title);
+			else if (searchParams.sort === 'titleza')
+				return b.title.localeCompare(a.title);
+			else
+				return 0;
+		}
 
-    const filterAndSort = (allPosts) => {
-      return posts.filter(({node}) => meetsFilter(node)).sort(comparePosts)
-    } 
+		const filterAndSort = (allPosts) => {
+			return posts.filter(({ node }) => meetsFilter(node)).sort(comparePosts)
+		}
 
-    const onSelect = (data) => {
-      searchParams.sort = data.value; 
-      let new_query = this.props.location.origin + this.props.location.pathname + "?" + QueryString.stringify({tags: searchParams.tags, sort: searchParams.sort}, {arrayFormat: 'comma'});
-      navigate(new_query);
-    }
+		const onSelect = (data) => {
+			searchParams.sort = data.value;
+			let new_query = `/blog/` + "?" + QueryString.stringify({ tags: searchParams.tags, sort: searchParams.sort }, { arrayFormat: 'comma' });
+			console.log(searchParams);
+			console.log("Moving to query " + new_query + " based on search params");
+			navigate(new_query);
+		}
 
-    return (
-      <Layout location={this.props.location}>
-        <SEO title="Updates"
-          description="Browse articles published by Blankets for T.O., including summaries of past initiatives, photos of past events, and everything else associated with us." />
-        <div className="white-background">
-          <div style={{ padding: "20px" }}></div>
-          <div className="wrapper">
-            <h2 className={styles.sectionHeadline}>All Updates and Articles</h2>
-            <SearchTools onDropdownChange={onSelect} dropdownPlaceholder={searchParams.sort} tags={uniqueTags} clickTagFunc={clickTag} activeTags={searchParams.tags}/>
-            <div className={styles.blog_list}>
-              {filterAndSort(posts).map(({ node }) => {
-                return (
-                  <div key={node.slug}>
-                    <ArticleEntry article={node} />
-                  </div>
-                )
-              })}
-            </div>
-          </div>
-        </div>
-      </Layout>
-    )
-  }
+		const onClickTag = (clicked) => {
+			if (searchParams.tags.includes(clicked)) // if unselecting 
+				searchParams.tags = searchParams.tags.filter(tag => (tag != clicked));
+			else
+				searchParams.tags.push(clicked);
+			let new_query = `/blog/` + "?" + QueryString.stringify({ tags: searchParams.tags, sort: searchParams.sort }, { arrayFormat: 'comma' });
+			console.log(searchParams);
+			console.log("Moving to query " + new_query + " based on search params");
+			navigate(new_query);
+		}
+
+		return (
+			<Layout location={this.props.location}>
+				<SEO title="Updates"
+					description="Browse articles published by Blankets for T.O., including summaries of past initiatives, photos of past events, and everything else associated with us." />
+				<div className="white-background">
+					<div className="wrapper">
+						<h2>All Updates and Articles</h2>
+						<SearchTools onDropdownChange={onSelect} dropdownPlaceholder={searchParams.sort} tags={uniqueTags} clickTagFunc={onClickTag} activeTags={searchParams.tags} />
+						<div className={styles.blog_list}>
+							{filterAndSort(posts).map(({ node }) => {
+								return (
+									<div key={node.slug}>
+										<ArticleEntry article={node} />
+									</div>
+								)
+							})}
+						</div>
+					</div>
+				</div>
+			</Layout>
+		)
+	}
 }
 
 export default BlogIndex
 
 export const blogPageQuery = graphql`
-        query BlogIndexQuery {
-          site {
-            siteMetadata {
-              title
-            }
-          }
-          allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }, limit: 20, filter: {articleType: {ne: "Page"}}) {
-            edges {
-              node {
-                title
-                slug
-                richTextBody {
-                  json
-                }
-                publishDateValue : publishDate
-                publishDate(formatString: "MMMM Do, YYYY")      
-                description {
-                  childMarkdownRemark {
-                    html
-                  }
-                }
-                tags
-                imagePreview {
-                  fixed(
-                    width: 300
-                    height: 300
-                    quality: 0
-                    resizingBehavior: FILL
-                    background: "rgb:000000"
-                    ) {
-                      src
-                    }
-                  description
-                }
-              }
-            }
-          }
-        }
-        `
+	query BlogIndexQuery {
+		site {
+			siteMetadata {
+				title
+			}
+		}
+		allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }, limit: 20, filter: {articleType: {ne: "Page"}}) {
+			edges {
+				node {
+					title
+					slug
+					richTextBody {
+						json
+					}
+					publishDateValue : publishDate
+					publishDate(formatString: "MMMM Do, YYYY")      
+					description {
+						childMarkdownRemark {
+							html
+						}
+					}
+					tags
+					imagePreview {
+						fixed(
+							width: 300
+							height: 300
+							quality: 0
+							resizingBehavior: FILL
+							background: "rgb:000000"
+							) {
+								src
+							}
+							description
+						}
+					}
+				}
+			}
+		}
+`
 
-        //   heroImage {
-        //     fluid(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
-        //       ...GatsbyContentfulFluid_tracedSVG
-        //     }
-        //   }
+			//   heroImage {
+			//     fluid(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
+			//       ...GatsbyContentfulFluid_tracedSVG
+			//     }
+			//   }
