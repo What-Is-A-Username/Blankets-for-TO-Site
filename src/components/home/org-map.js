a84dba0cf17bdc16bf617151fa281df2f51d87a7
What-Is-A-Username <49571870+What-Is-A-Username@users.noreply.github.com>
2021-08-09 11:22:17 -0400

    Map prototype

--- src/components/home/org-map.js
+++ src/components/home/org-map.js
@@ -1,58 +1,120 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState } from 'react'
+import get from 'lodash/get'
 import { Map, Marker } from 'pigeon-maps'
 import { osm } from 'pigeon-maps/providers'
 import turfBbox from '@turf/bbox'
 import { featureCollection as turfFeatureCollection, point as turfPoint } from '@turf/helpers'
 import geoViewport from '@mapbox/geo-viewport'
 import styles from '../home/org-map.module.css'
+import { StaticQuery } from 'gatsby'
 
 export default () => { 
 
-	const height = 800; 
+	const height = 400; 
 	const width = 400;
-	// const mapRef = useRef(null) 
+	var numLocations = 0;
+	const [selectedLocation, setSelectedlocation] = useState(0)
+	const [mapCenter, setCenter] = useState(-1)
 
-	const locations = [[43.853, -79.081111], [43.654925, -79.640207], [44.044940, -79.469373]]
+	const locationQuery = graphql`
+	query LocationQuery {
+		allContentfulDonationLocation {
+			nodes {
+				city
+				coordinateLatitude
+				coordinateLongitude
+				name
+				provinceState
+				street
+				description {
+				description
+				}
+			}
+		}
+	}
+	`
+	
+	const onMarkerClick = (locationIndex) => { 
+		setCenter(locationIndex)
+		setSelectedlocation(locationIndex)
+	}
+
+	const onNext = () => {
+		let newIndex = (selectedLocation+1) % numLocations
+		setSelectedlocation(newIndex)
+		setCenter(newIndex)
+	}
+
+	const onLast = () => {
+		let newIndex = selectedLocation-1
+		if (newIndex < 0) newIndex += numLocations
+		setSelectedlocation(newIndex)
+		setCenter(newIndex)
+	}
 	
 	const centerZoomFromLocations = (givenLocs) => {
-		console.log("recalculating with h:w " + height + " " + width)
 		const points = givenLocs.map(givenLocs => turfPoint([givenLocs[0], givenLocs[1]]))
 		const features = turfFeatureCollection(points)
 		const bounds = turfBbox(features)
-		return geoViewport.viewport(bounds, [height, width], 1, 13, 256)
+		return geoViewport.viewport(bounds, [height, width], 1, 25, 256)
 	}
-	
 
-	const zoomSettings = centerZoomFromLocations(locations); 
-	const mapObject = 
-	<Map 
-	  	// ref={mapRef}
-		height={height} 
-		boxClassname={"custom-pigeon"}
-		center={zoomSettings.center}
-		zoom={zoomSettings.zoom}
-		mouseEvents={false}
-		touchEvents={false}
-		attribution={false}>
+	const render = (mapLocations) => 
 	{
-		locations.map(x => 
-			{
-				return(
-					<Marker 
-						width={50}
-						anchor={x} 
-						color={'red'}
-					/>
+		{console.log(numLocations)}
+		numLocations = mapLocations.length; 
+		if (selectedLocation === undefined)
+		{
+			setSelectedlocation(mapLocations[0])
+			return;
+		}
+		const locations = mapLocations.map(x => { return([x.coordinateLatitude, x.coordinateLongitude])})
+		const zoomSettings = centerZoomFromLocations(locations); 
+		return(
+			<div className={styles.mapContainer}>
+				<Map 
+					height={height} 
+					boxClassname={"custom-pigeon"}
+					provider={osm}
+					defaultCenter={zoomSettings.center}
+					center={mapCenter === -1 ? mapCenter : locations[mapCenter]}
+					zoom={zoomSettings.zoom*1.3}
+					touchEvents={false}
+					attribution={false}
+				>
+					{console.log("center is " + locations[mapCenter])}
+				{
+					mapLocations.map((info, index) => 
+						{
+							return(
+								<Marker 
+									width={50}
+									anchor={[info.coordinateLatitude, info.coordinateLongitude]} 
+									color={index === selectedLocation ? 'red' : 'pink'}
+									onClick={() => onMarkerClick(index)}
+									/>
+									)
+								}
 					)
-			}
+				}
+				</Map>
+				<div className={styles.infoBox}>
+					<div onClick={onLast}>{'<'}</div>
+					<div className={styles.textBox}>
+						<h1 className={styles.infoBoxTitle}>{mapLocations[selectedLocation].name}</h1>
+						<p className={styles.infoBoxAddress}>{mapLocations[selectedLocation].street}, {mapLocations[selectedLocation].city}, {mapLocations[selectedLocation].provinceState} </p>
+						<p className={styles.infoBoxDescription}>{mapLocations[selectedLocation].description.description}</p>
+					</div>
+					<div onClick={onNext}>{'>'}</div>
+				</div>
+		</div>
 		)
 	}
-	</Map>
 			
 	return (
-		<div className={styles.mapContainer}>
-			{mapObject}
-			{console.log(mapObject)}
-		</div>
+		<StaticQuery query={locationQuery} 
+			render={data => 
+				render(data.allContentfulDonationLocation.nodes)}
+		/>
 	)
 }
