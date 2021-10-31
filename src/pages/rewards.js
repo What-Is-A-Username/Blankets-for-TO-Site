bcf44f900a2338332e38345c519ea0d8afdf6e76
What-Is-A-Username <49571870+What-Is-A-Username@users.noreply.github.com>
2021-10-31 14:06:19 -0400

    Updated rewards information

--- src/pages/rewards.js
+++ src/pages/rewards.js
@@ -57,58 +57,60 @@ export default class Store extends React.Component {
 				<SEO 
                     title='Member Rewards'
                     description='The Member Rewards system recognizes members for their involvement in the community. View recognized tasks and reward prizes here.'/>
-				<div className="white-background">
-					<div className="wrapper" styles={{width: '80%'}}>
+				<div className='white-background'>
+					<div className='wrapper'>
 						<h2>Member Rewards</h2>
                         <div className='richText'>
                             <p>
-                                The Blankets for T.O. Member Rewards system aims to recognize members for all the hard work and commitment they have dedicated to our mission of eradicating homelessness.
+                                The Blankets for T.O. Member Rewards System aims to recognize members for all the hard work and commitment they have dedicated to our mission of eradicating homelessness.
                             </p>
                             <p>
-                                This system is only available to Blankets for T.O. members. Information on how to become a member can be found <a href='/positions'>here</a>.
+                                This system is only available to Blankets for T.O. members. Information on how to become a member can be found on our <a href='/positions'>Membership page</a>.
                             </p>
                         </div>
                         <div id='info'>
-                            <h3 className={styles.taskTitle} style={{fontSize: '3.0em'}}> How does it work?</h3>
+                            <h3 className={styles.mainTitle}> How does it work?</h3>
                         </div>
-                        <div className={styles.instructionsList}>
-                            <Fade delay={0} left>
-                                <div className={styles.instructionPanel}>
-                                    <p className={styles.instructionNumber}>Step 1</p>
-                                    <CheckCircle size={instructionIconSize} strokeWidth={instructionIconStrokeWidth}/>
-                                    <p className={styles.instructionDescription}> 
-                                        Contribute to Blankets for T.O. by completing any of the designated tasks below.
-                                    </p>
-                                </div>
-                            </Fade>
-                            <Fade delay={300} left>
-                                <div className={styles.instructionPanel}> 
-                                    <p className={styles.instructionNumber}>Step 2</p>
-                                    <Send size={instructionIconSize} strokeWidth={instructionIconStrokeWidth}/>
-                                    <p className={styles.instructionDescription}>
-                                        Verify completed tasks by providing supporting proof like screenshots or files to our Executive Team so that we can record it.
-                                    </p >
-                                </div>
-                            </Fade>
-                            <Fade delay={600} left>
-                                <div className={styles.instructionPanel}>
-                                    <p className={styles.instructionNumber}>Step 3</p>
-                                    <BarChart size={instructionIconSize} strokeWidth={instructionIconStrokeWidth}/>
-                                    <p className={styles.instructionDescription}>
-                                        Continue completing tasks and gathering points. Use our spreadsheet, updated by our Executive Team, to keep updated on your points total!
-                                    </p>
-                                </div>
-                            </Fade>
-                            <Fade delay={900} left>
-                                <div className={styles.instructionPanel}>
-                                    <p className={styles.instructionNumber}>Step 4</p>
-                                    <Gift size={instructionIconSize} strokeWidth={instructionIconStrokeWidth}/>
-                                    <p className={styles.instructionDescription}>
-                                        Spend your rewards points to redeem for our prizes, which include drones, cameras, and speakers!
-                                    </p>
-                                </div>
-                            </Fade>
-                        </div>
+                    </div>
+                    <div className={styles.instructionsList}>
+                        <Fade delay={0} left>
+                            <div className={styles.instructionPanel}>
+                                <p className={styles.instructionNumber}>Step 1</p>
+                                <CheckCircle size={instructionIconSize} strokeWidth={instructionIconStrokeWidth}/>
+                                <p className={styles.instructionDescription}> 
+                                    Contribute to Blankets for T.O. by completing any of the designated tasks below.
+                                </p>
+                            </div>
+                        </Fade>
+                        <Fade delay={300} left>
+                            <div className={styles.instructionPanel}> 
+                                <p className={styles.instructionNumber}>Step 2</p>
+                                <Send size={instructionIconSize} strokeWidth={instructionIconStrokeWidth}/>
+                                <p className={styles.instructionDescription}>
+                                    Verify completed tasks by providing supporting proof like screenshots or files to our Executive Team so that we can record it.
+                                </p >
+                            </div>
+                        </Fade>
+                        <Fade delay={600} left>
+                            <div className={styles.instructionPanel}>
+                                <p className={styles.instructionNumber}>Step 3</p>
+                                <BarChart size={instructionIconSize} strokeWidth={instructionIconStrokeWidth}/>
+                                <p className={styles.instructionDescription}>
+                                    Continue completing tasks and gathering points. Use our spreadsheet, updated by our Executive Team, to keep updated on your points total!
+                                </p>
+                            </div>
+                        </Fade>
+                        <Fade delay={900} left>
+                            <div className={styles.instructionPanel}>
+                                <p className={styles.instructionNumber}>Step 4</p>
+                                <Gift size={instructionIconSize} strokeWidth={instructionIconStrokeWidth}/>
+                                <p className={styles.instructionDescription}>
+                                    Spend your rewards points to redeem for our prizes, which include drones, cameras, and speakers!
+                                </p>
+                            </div>
+                        </Fade>
+                    </div>
+                    <div className='wrapper'>
                         <div id='tasks'>
                             <h3 className={styles.taskTitle}>What tasks can I complete?</h3>
                         </div>
@@ -120,10 +122,10 @@ export default class Store extends React.Component {
                             }
                         </div>
                         <p className={styles.notes}>** This activity can only be done once by a member each month</p>
-                        <div id='submission' style={{textAlign:'center'}}>
+                        <div id='submission'>
                             <h3 className={styles.taskTitle}>How do I get my tasks verified?</h3>
-                            <p style={{fontSize:'18px'}}>In order to be rewarded points for each task you complete, you must submit supporting proof of the task completion. This may include screenshots, digital media or written material.</p>
-                            <p style={{fontWeight: 'bold', fontSize:'18px'}}>To submit tasks for the months of October 2021 and November 2021, please follow the link below to fill out the Google Form:</p>
+                            <p className={styles.infoText}>In order to be rewarded points for each task you complete, you must submit supporting proof of the task completion. This may include screenshots, digital media or written material.</p>
+                            <p className={styles.infoTextBold}>To submit tasks for the months of October 2021 and November 2021, please follow the link below to fill out the Google Form:</p>
                             <StyledButton buttonText='Submit task for verification' link={taskVerificationLink} openInNewTab/> 
                             <a></a>
                         </div>
@@ -137,10 +139,22 @@ export default class Store extends React.Component {
                                 }
                             )}
                         </div>
-                        <div id='check' style={{textAlign:'center'}}>
+                        <div id='check'>
                             <h3 className={styles.taskTitle}>How can I check my points?</h3>
-                            <p style={{fontSize:'18px'}}>
-                                View your points by opening our spreadsheet document, which is currently still under construction. This document will be maintained by our Executive Team. Additionally, we are currently working on allowing members of our Discord server to specialized bot commands to view their points total.
+                            <p className={styles.infoTextBold}>
+                                The master points tracker can be accessed via the button below:
+                            </p>
+                            <StyledButton buttonText='View Rewards Points' link='https://docs.google.com/spreadsheets/d/14XNz6xy8jVBHcaRmDPyR_LqipE8e6zQyBN7gAugqALo' openInNewTab/>
+                            <p className={styles.infoText}>Kindly allow our executive team enough time to update your points after you have sent a verification request. Verifications are normally processed at the end of each month.</p>
+                            <p className={styles.infoTextBold}>
+                                Discord server members can also view their points through the our server's custom Discord bot:
+                            </p>
+                            <p className={styles.infoText}>
+                                Simply send a message with "!help" in the #bot-commands channel or in a private message to view relevant commands. Ask our Bot Developer, William, if you have any questions or issues with the bot!
+                            </p>
+                            <h3 className={styles.taskTitle}>Who can I ask for questions?</h3>
+                            <p className={styles.infoText}>
+                                For any questions and concerns about the rewards system or your personal points tally specifically, contact us through email, social media or Discord. 
                             </p>
                         </div>
 					</div>
