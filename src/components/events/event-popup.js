bb3973da5901adb95858c09b3521d6dbde57cae0
What-Is-A-Username <49571870+What-Is-A-Username@users.noreply.github.com>
2023-09-01 00:43:10 -0400

    Allow events with no articles

--- src/components/events/event-popup.js
+++ src/components/events/event-popup.js
@@ -7,61 +7,64 @@ import { X } from "react-feather";
 class EventPopup extends React.Component {
 
     render() {
+        const { eventData, onClose } = this.props;
 
-        const {eventData, onClose} = this.props;
-  
-        eventData.articles = eventData.articles.map(article => {
-            const { sys: { contentType: { sys: { id: entryType }} } } = article
+        const articles = eventData.articles ? eventData.articles.map(article => {
+            const { sys: { contentType: { sys: { id: entryType } } } } = article
             if (entryType === 'blogPost') {
-                return {...article, type: article.articleType, fullSlug: `/blog/${article.slug}` }
+                return { ...article, type: article.articleType, fullSlug: `/blog/${article.slug}` }
             } else if (entryType === 'page') {
-                return {...article, type: 'Page', fullSlug: `/pages/${article.slug}`}
+                return { ...article, type: 'Page', fullSlug: `/pages/${article.slug}` }
             } else {
-                return {...article, type: 'Link', fullSlug: `/${slug}`}
+                return { ...article, type: 'Link', fullSlug: `/${slug}` }
             }
+        }) : []
 
-        })
-
-        return(
+        return (
             <div className={background}>
-                <div className={blocker} onClick={onClose} ref={this.popupBlocker}/>
+                <div className={blocker} onClick={onClose} ref={this.popupBlocker} />
                 <div ref={this.modalRef} className={modal}>
                     <div className={modalPadding}>
                         <div className={modalContent}>
                             <h1 className={title}>{eventData.eventName}</h1>
                             <div className={modalLayout}>
                                 <div className={image}>
-                                    <GatsbyImage alt={eventData.smallPreviewImage.description} image={eventData.smallPreviewImage.gatsbyImageData}/>
+                                    <GatsbyImage alt={eventData.smallPreviewImage.description} image={eventData.smallPreviewImage.gatsbyImageData} />
                                 </div>
                                 <div className={textContent}>
                                     <div className={description} dangerouslySetInnerHTML={{
                                         __html: eventData.description.childMarkdownRemark.html,
-                                    }}/>
+                                    }} />
                                     <strong className={subtitle}>When is it?</strong>
                                     <p className={datetime}>
                                         {
-                                            eventData.eventDate !== `Date and time to be announced` ? 
-                                            `${eventData.eventDate} to ${eventData.endDate}` :
-                                            `Date and time to be announced.`
+                                            eventData.eventDate !== `Date and time to be announced` ?
+                                                `${eventData.eventDate} to ${eventData.endDate}` :
+                                                `Date and time to be announced.`
                                         }
                                     </p>
                                     <strong className={subtitle}>Where is it?</strong>
                                     <div className={location} dangerouslySetInnerHTML={{
                                         __html: eventData.location.childMarkdownRemark.html,
-                                    }}/>
-                                    <strong className={subtitle}>Read more</strong>
+                                    }} />
                                     {
-                                        eventData.articles.map((article) => {
-                                            return(
-                                                <div key={article.fullSlug}>
-                                                    <span>({article.type}) <a className={articleLink} href={article.fullSlug}>{article.title}</a></span>
-                                                </div>
-                                            )
-                                        })
+                                        articles.length > 0 &&
+                                        <>
+                                            <strong className={subtitle}>Read more</strong>
+                                            {
+                                                eventData.articles.map((article) => {
+                                                    return (
+                                                        <div key={article.fullSlug}>
+                                                            <a className={articleLink} href={article.fullSlug}>{article.title}</a>
+                                                        </div>
+                                                    )
+                                                })
+                                            }
+                                        </>
                                     }
                                 </div>
                             </div>
-                            <X className={closeButton} onClick={onClose} size={40}/>
+                            <X className={closeButton} onClick={onClose} size={40} />
                         </div>
                     </div>
                 </div>
